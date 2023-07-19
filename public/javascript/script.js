d9a2549a40d37381c0ea5c4d61972071a23d7a88
Nurislom3000 <120260385+Nurislom3000@users.noreply.github.com>
2023-07-19 12:53:05 +0500

    Сайт для ресторана

---
+++ public/javascript/script.js
@@ -0,0 +1,6 @@
+const did = document.getElementById('nav')
+
+function togle() {did.classList.toggle('show')}
+function desert() {did.classList.remove('show')}
+const search_wrapper = document.querySelector('.search_wrapper')
+function recolink() {search_wrapper.classList.toggle('open')}
